4b47f3b612ed60fbf7d2ccab5a4d299882500338
Manoj Karthick <smanojkarthick@gmail.com>
2021-06-30 00:25:03 -0700

    Create release v0.3.3

--- Formula/reddsaver.rb
+++ Formula/reddsaver.rb
@@ -2,8 +2,8 @@ class Reddsaver < Formula
   desc "CLI tool to download saved media from Reddit "
   homepage "https://github.com/manojkarthick/reddsaver"
   url "https://github.com/manojkarthick/homebrew-reddsaver/releases/latest/download/reddsaver-mac.tar.gz"
-  version "0.3.2"
-  sha256 "2089579e1a4696f180e76dfbc6b00aeeaf2e303454d779b942066c0ab1bda571"
+  version "0.3.3"
+  sha256 "d65a202d81bab4979a10624835989c48cf3becc3b9ac780077ce11a7da768e3c"
 
   def install
     bin.install "reddsaver"
